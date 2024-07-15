ad04536898c82dad5fba4df0b57c0d255e3ea90b
Thomas Rohde <thomas.rohde@protonmail.com>
2024-07-15 16:31:25 +0200

    Added linebreaks

--- R/downloader.R
+++ R/downloader.R
@@ -4,9 +4,10 @@
 
 downloader <- \(.url, .file, .method = "curl", verbose = T) {
 
-  message(ifelse(file.exists(.file),
-                 base::paste0("File ", .file, " exists. Skipping."),
-                 base::paste0("File ", .file, " does not exist. Downloading...")))
+  message(
+    ifelse(file.exists(.file),
+           base::paste0("File ", .file, " exists. Skipping."),
+           base::paste0("File ", .file, " does not exist. Downloading...")))
   if (!file.exists(.file)) {
     utils::download.file(.url, .file, method = .method, quiet = !verbose)
   }
--- R/import_depmap.R
+++ R/import_depmap.R
@@ -8,9 +8,10 @@ import_depmap <- \(file, impute = F, verbose = T) {
   colnames(object) <- str_split_i(colnames(object), " \\(", 1)
   if (impute) {
     if (verbose) {
-      base::message(base::paste0(na_perc(object)*100, " percent of values are imputed."))
+      base::message(base::paste0(na_perc(object)*100,
+                                 " percent of values are imputed."))
       }
-    for (i in 1:ncol(object)) {object[,i][is.na(object[,i])] <- mean(object[,i], na.rm = T)}}
+    for (i in 1:ncol(object)) {
+      object[,i][is.na(object[,i])] <- mean(object[,i], na.rm = T)}}
   return(object)
 }
-
